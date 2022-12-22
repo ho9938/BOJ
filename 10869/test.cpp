74f3d3fcdb7397c9a8776c3a70690d95a0e3caf2
Yoon Changho <ho9938@unist.ac.kr>
2022-12-22 14:38:11 +0900

    10869 solved

---
+++ 10869/test.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a, b;
+    double aa, bb;
+    cin >> a >> b;
+    aa = (double)a;
+    bb = (double)b;
+
+    cout << a + b << endl;
+    cout << a - b << endl;
+    cout << a * b << endl;
+    cout << a / b << endl;
+    cout << a % b;
+
+    return 0;
+}
