1eb21aa3a4925312b334909eace8a530c639f8bc
Yoon Changho <ho9938@unist.ac.kr>
2022-12-24 15:59:37 +0900

    25083 solved with cpp

---
+++ 25083/test.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    string sprout = "         ,r'\"7\n"
+                    "r`-_   ,'  ,/\n"
+                    " \\. \". L_r'\n"
+                    "   `~\\/\n"
+                    "      |\n"
+                    "      |";
+
+    cout << sprout;
+    return 0;
+}
