729f1c24c299cae25036c6a19beb75c9b116e37a
Yoon Changho <ho9938@unist.ac.kr>
2022-12-24 15:47:22 +0900

    10171 solved with cpp

---
+++ 10171/test.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    string cat = "\\    /\\\n"
+                 " )  ( ')\n"
+                 "(  /  )\n"
+                 " \\(__)|";
+
+    cout << cat;
+
+    return 0;
+}
