b833e8798a545dd82c8ed01cfbbd5631a431290c
Yoon Changho <ho9938@unist.ac.kr>
2023-02-01 23:00:49 +0900

    10872 solved with cpp

---
+++ 10872/test.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    int fact = 1;
+    for (int i = n; i > 0; i--)
+        fact *= i;
+
+    cout << fact << endl;
+
+    return 0;
+}
